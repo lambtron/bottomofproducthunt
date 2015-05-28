4ad306528b24367fb867d50f33383a621795808e
Andy Jiang <andyjiang@gmail.com>
2015-05-27 22:34:10 -0700

    updated server/lib/routes.js

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -3,12 +3,16 @@
  * Module dependencies.
  */
 
+var ProductHunt = require('./producthunt');
 var render = require('./render');
+var PH = require('./ph');
 
 /**
  * Render index html page.
  */
 
 exports.index = function *() {
-  return this.body = yield render('index');
+  var token = yield PH.token();
+  var body = yield ProductHunt.posts(token);
+  return this.body = yield render('index', body);
 };
